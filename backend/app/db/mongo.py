7371d6238976ab3e1e3b0368d07facdd128f154b
BuddhiDihansa <buddhidihansa@gmail.com>
2026-09-12 11:02:31 +0530

    mongo db update

--- backend/app/db/mongo.py
+++ backend/app/db/mongo.py
@@ -1,8 +1,9 @@
+import certifi
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from app.core.config import settings
 
-client = AsyncIOMotorClient(settings.mongodb_uri)
+client = AsyncIOMotorClient(settings.mongodb_uri, tlsCAFile=certifi.where())
 db = client.get_default_database()
 
 users = db["users"]
